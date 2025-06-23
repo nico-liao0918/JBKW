a11823026ddbec9a8c94d291cae4dce5d780c298
nico-liao0918 <40799837+nico-liao0918@users.noreply.github.com>
2025-06-23 12:59:40 +0800

    Update dashboard.js

--- maintain/dashboard.js
+++ maintain/dashboard.js
@@ -10,25 +10,6 @@ async function loadDashboard() {
   if (!username) return logout();
 
   document.getElementById("welcome").textContent = `👋 Hello, ${username}`;
-
-  try {
-    const res = await fetch("purchases.json");
-    const data = await res.json();
-
-    const userData = data.find(u => u.Username === username);
-
-    if (userData && userData.Items.length > 0) {
-      userData.Items.forEach(item => {
-        const li = document.createElement("li");
-        li.textContent = item;
-        document.getElementById("purchaseList").appendChild(li);
-      });
-    } else {
-      document.getElementById("purchaseList").innerHTML = "<li>No purchases yet.</li>";
-    }
-  } catch (err) {
-    console.error("Error loading purchase data", err);
-  }
 }
 
 loadDashboard();
